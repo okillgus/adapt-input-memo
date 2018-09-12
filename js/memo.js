93977a5be69f21f57b82a882324dbdfbd0410491
okillgus <40393122+okillgus@users.noreply.github.com>
2018-09-12 09:36:14 +0200

    Create memo.js
    init

---
+++ js/memo.js
@@ -0,0 +1,13 @@
+# control
+
+define(["coreJS/adapt", "coreViews/componentView"], function(Adapt, ComponentView) {
+  var MemoView =  = ComponentView.extend({
+        initialize: function(){},
+        preRender:  function(){},
+        postRender: function(){},
+        preRemove:  function(){}
+  });
+
+    Adapt.register("memo", MemoView);
+    return MemoView;
+});
