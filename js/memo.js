0342d74d5d19661cfa8bba203ba4e217f4449232
Oliver Killgus <o.killgus@alp.dillingen.de>
2018-09-21 13:04:09 +0200

    nn

--- js/memo.js
+++ js/memo.js
@@ -1,7 +1,7 @@
 # control
 
 define(["coreJS/adapt", "coreViews/componentView"], function(Adapt, ComponentView) {
-  var MemoView =  = ComponentView.extend({
+  var MemoView = ComponentView.extend({
         initialize: function(){},
         preRender:  function(){},
         postRender: function(){},
