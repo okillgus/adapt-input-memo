e296cc3799143ab5042a5d3e8c0588a76a3271ca
Oliver Killgus <o.killgus@alp.dillingen.de>
2018-10-02 18:52:32 +0200

    erster erfolgreicher  import, läuft: js fixed

--- js/memo.js
+++ js/memo.js
@@ -1,27 +1,25 @@
-// control
+define(function(require) {
 
-define(["coreJS/adapt", "coreViews/componentView"], function(Adapt, ComponentView) {
-  var MemoView = ComponentView.extend({
-        initialize: function(){
-          // ???
-        },
-        preRender:  function(){
-          // Daten aus DB: topic, inputId
-          // Objekt konstruieren
-          // memoText belegen 
-        },
-        postRender: function(){
-          // css? 
+  var ComponentView = require('coreViews/componentView');
+  var Adapt = require('coreJS/adapt');
+
+  var Memo = ComponentView.extend({
+
+      preRender: function() {
+          // this.checkIfResetOnRevisit();
+      },
+
+      postRender: function() {
           this.setReadyStatus();
-        },
-        preRemove:  function(){
-          // Daten sichern ?
-        }
-  },
-    {
-        template: 'memo'
-    });
 
-    Adapt.register("memo", MemoView);
-    return MemoView;
+          // this.setupInview();
+      }
+  },
+  {
+      template: 'memo'
+  });
+
+  Adapt.register('memo', Memo);
+
+  return Memo;
 });
